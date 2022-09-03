79d2e62fc1b7ea936442aeb780b0ecebfe0d7e44
iRealWorlds <marincalin2551@hotmail.com>
2022-09-03 23:15:58 +0300

    The payment controller no longer uses model binding.

--- src/Http/PaymentController.php
+++ src/Http/PaymentController.php
@@ -4,6 +4,7 @@ namespace Codestage\Netopia\Http;
 
 use Codestage\Netopia\Models\Payment;
 use Exception;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
 class PaymentController
@@ -11,12 +12,17 @@ class PaymentController
     /**
      * Redirect the user to a payment URL.
      *
-     * @param Payment $payment
+     * @param Request $request
      * @throws Exception
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Payment $payment): \Illuminate\Http\Response
+    public function __invoke(Request $request): \Illuminate\Http\Response
     {
+        $id = $request->route('payment');
+
+        /** @var Payment $payment */
+        $payment = Payment::query()->findOrFail($id);
+
         return Response::view('netopia::execute_payment', [
             'payment' => $payment->encrypt()
         ]);
