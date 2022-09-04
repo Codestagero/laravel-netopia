1a034a89cc17febc1fcbec008d909b80b630900c
iRealWorlds <marincalin2551@hotmail.com>
2022-09-04 17:57:14 +0300

    Payments that have already been completed can no longer be reinitiated.

--- src/Http/Controllers/PaymentController.php
+++ src/Http/Controllers/PaymentController.php
@@ -2,10 +2,12 @@
 
 namespace Codestage\Netopia\Http\Controllers;
 
+use Codestage\Netopia\Enums\PaymentStatus;
 use Codestage\Netopia\Models\Payment;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class PaymentController
 {
@@ -22,6 +24,11 @@ class PaymentController
 
         /** @var Payment $payment */
         $payment = Payment::query()->findOrFail($id);
+
+        // Check if this payment needs more steps
+        if (!\in_array($payment->status, [PaymentStatus::NotStarted, PaymentStatus::Rejected])) {
+            throw new HttpException(403);
+        }
 
         return Response::view('netopia::execute_payment', [
             'payment' => $payment->encrypt()
