041de1e2eb75f6f948c22e207fcdf3a1877c556b
iRealWorlds <marincalin2551@hotmail.com>
2022-09-04 16:31:26 +0300

    Updated the payment_success view.

--- resources/views/payment_success.blade.php
+++ resources/views/payment_success.blade.php
@@ -10,17 +10,17 @@
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-w-screen min-h-screen">
     <div class="p-6 bg-white border shadow rounded-lg">
-        <h1 class="text-xl">
+        <h1 class="text-2xl">
             Payment complete
         </h1>
 
-        <p>
+        <p class="mt-4 text-gray-500 text-sm">
             You have successfully completed the payment.
             You can now safely close this tab
         </p>
 
-        <div class="flex items-center justify-center">
-            <button class="rounded-lg px-4 py-2 bg-gray-900 text-gray-100" onclick="window.close()">
+        <div class="flex items-center justify-center mt-7">
+            <button class="rounded-lg px-4 py-2 hover:bg-gray-900 bg-gray-800 text-gray-100" onclick="window.close()">
                 Close window
             </button>
         </div>
