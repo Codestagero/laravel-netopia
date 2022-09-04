614a8d1f24bb223bbc3a653ab789823d13182ba2
iRealWorlds <marincalin2551@hotmail.com>
2022-09-04 17:54:54 +0300

    Added an initial payment status.

--- database/migrations/2022_09_03_205639_create_payments_table.php
+++ database/migrations/2022_09_03_205639_create_payments_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
     {
         Schema::create('netopia_payments', function (Blueprint $table): void {
             $table->string('id')->primary();
-            $table->string('status')->default(PaymentStatus::Pending->value);
+            $table->string('status')->default(PaymentStatus::NotStarted->value);
             $table->decimal('amount');
             $table->string('currency', 6);
             $table->text('description')->nullable()->default(null);
--- src/Enums/PaymentStatus.php
+++ src/Enums/PaymentStatus.php
@@ -4,10 +4,38 @@ namespace Codestage\Netopia\Enums;
 
 enum PaymentStatus: string
 {
+    /**
+     * The billable entity has not yet done any action to complete this payment.
+     */
+    case NotStarted = 'not_started';
+
+    /**
+     * This payment was confirmed as successful.
+     */
     case Confirmed = 'confirmed';
+
+    /**
+     * The billable entity has moved to complete this payment, but it is still pending.
+     */
     case Pending = 'pending';
+
+    /**
+     * The payment has been pre-authorized.
+     */
     case Preauthorized = 'preauthorized';
+
+    /**
+     * The payment has been cancelled.
+     */
     case Cancelled = 'cancelled';
+
+    /**
+     * The payment has been refunded.
+     */
     case Refunded = 'refunded';
+
+    /**
+     * The payment has been rejected by the processing service.
+     */
     case Rejected = 'rejected';
 }
